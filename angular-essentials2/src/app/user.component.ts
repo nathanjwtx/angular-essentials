060a9315b83b7b5e62c6c3b10c4c8cf1757d6acd
Nathan <nathanjwtx@nathanjwtx.io>
2019-03-29 15:24:24 -0500

    Added two way binding of property

--- angular-essentials2/src/app/user.component.ts
+++ angular-essentials2/src/app/user.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 
+/* alternative property binding syntax value="{{ name }}"
+<input type="text" (input)="onUserInput($event)" [value]="name">
+*/
+
 @Component({
   selector: 'app-user',
   template: `
-  <input type="text" (input)="onUserInput($event)">
+  <input type="text" [(ngModel)]="name">
   <p>Hello! {{ name }}</p>
   <p>I'm the user component</p>`
 })
