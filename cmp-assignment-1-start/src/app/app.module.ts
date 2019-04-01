e2284878227034a095eb631743715a70aeea0b08
Nathan Waterman <nathanjwtx@nathanjwtx.io>
2019-04-01 08:23:46 -0500

    Completed first assignment

--- cmp-assignment-1-start/src/app/app.module.ts
+++ cmp-assignment-1-start/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from "./dashboard.component";
@@ -10,7 +11,8 @@ import { DashboardComponent } from "./dashboard.component";
     DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- cmp-assignment-1-start/src/app/dashboard.component.ts
+++ cmp-assignment-1-start/src/app/dashboard.component.ts
@@ -2,12 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-dashboard',
-    template:`
-    <p>Hello {{ name }}! This is my new dashboard component!
-    <p>Current status of dashboard is: {{ loadState }}`
+    templateUrl: './dashboard.component.html',
+    // template:`
+    // <p>Hello {{ name }}! This is my new dashboard component!
+    // <p>Current status of dashboard is: {{ loadState }}
+    // <div>
+    //     <button (click)="onSwitchState()">Switch state</button>
+    // </div>
+    // <div>
+    //     <input type="text" [(ngModel)]="loadState">
+    // </div>`
 })
 
 export class DashboardComponent {
     name = 'Nathan';
     loadState = 'loading';
+
+    onSwitchState() {
+        this.loadState = 'Finished!';
+    }
 }
