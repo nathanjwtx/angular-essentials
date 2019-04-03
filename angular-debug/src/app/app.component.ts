9c0572906b46c84c3d0d2a061f26f5cdcbc14f05
Nathan <nathanjwtx@nathanjwtx.io>
2019-04-03 13:59:20 -0500

    Angular Debug completed

--- angular-debug/src/app/app.component.ts
+++ angular-debug/src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+/* in order to use the line below, types must be imported using npm install --save @types/lodash
+then random can be used as shown below */
+import { random } from 'lodash';
+
+/* below is used when using traditional javascript importing, e.g. import 'lodash';
+then this.number = _.random(1, 10); */
+// declare var _: any;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  number = 0;
+
+  onIncrease() {
+    // this.number = this.number * 2;
+    this.number = random(1, 10);
+  }
 }
